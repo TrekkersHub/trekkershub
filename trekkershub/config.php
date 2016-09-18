4f6d9700457f0975e9bc0b2596a54d84f2745769
vkrmsngh43 <vikram@catchthatbus.com>
2016-09-18 17:44:26 +0800

    Adding new files

---
+++ trekkershub/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vikram
+ * Date: 16/09/2016
+ * Time: 8:42 PM
+ */
+
+return array(
+  'host' => '',
+    'username' => '',
+    'password'=> '',
+    'dbName' => '',
+);
+?>
---
+++ trekkershub/trek-search.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vikram
+ * Date: 16/09/2016
+ * Time: 8:55 PM
+ */
+
+$selectedRegion = $_POST['treksRegion'];
+
+$selectedTrek = $_POST['treks'];
+
+$departDate = $_POST['departDate'];
+
+echo "Selected Region :".$selectedRegion. " Selected Trek :".$selectedTrek. " DaprtDate :". $departDate;
+?>
